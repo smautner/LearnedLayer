24d4815d8ea17bb36540d90b8f0f9a672232332d
S Mautner <mautner@cs.uni-freiburg.de>
2017-03-23 17:47:33 +0100

    asd

--- layerutils_rna.py
+++ layerutils_rna.py
@@ -1,7 +1,7 @@
 from graphlearn.minor.rna.rnadecomposer import RnaDecomposer
 import graphlearn.minor.rna.infernal as infernal
 from graphlearn.minor.rna import forgitransform as forgitransform
-
+from graphlearn.learnedlayer import cascade
 def make_samplers_rna():
 
     # default sampla
@@ -16,7 +16,7 @@ def make_samplers_rna():
                             #feasibility_checker=feasibility
                             include_seed=False
                            )
-
+    samplers=[sampler]
     # forgi transformer
     # check if calc_contracted_edge_nodes false is not breaking things
     sampler=infernal.AbstractSampler(
@@ -30,8 +30,21 @@ def make_samplers_rna():
                             #feasibility_checker=feasibility
                             include_seed=False
                            )
+    samplers.append(sampler)
+    sampler=infernal.AbstractSampler(
+                            #radius_list=[0,1],
+                            #thickness_list=[2],
+                            #min_cip_count=1,
+                            #min_interface_count=2,
+                            graphtransformer= cascade.RNACascade(),
+                            decomposer=RnaDecomposer(output_sequence=True,pre_vectorizer_rm_f=True,calc_contracted_edge_nodes=False),
+                            #estimator=estimator
+                            #feasibility_checker=feasibility
+                            include_seed=False
+                           )
 
-    # learned samplari!
+    samplers.append(sampler)
+    return samplers
     
 
 
