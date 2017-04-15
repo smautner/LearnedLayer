50cf6b035241b05a32ebd46f978d524a3a07ea70
S Mautner <mautner@cs.uni-freiburg.de>
2017-04-15 16:27:17 +0200

    hayaku!

---
+++ rnatest.py
@@ -0,0 +1,25 @@
+
+from rnalayer import get_data2
+from layerutils_rna import make_samplers_rna
+
+from eden.util import configure_logging
+import logging
+configure_logging(logging.getLogger(),verbosity=2)
+
+
+
+
+def run_experiments(data,samplers):
+    samplers=[samplers[1]]
+    return [[[[ list(sampler.fit_transform (problem))]
+        for problem in repeat]
+            for repeat in  data] 
+                for sampler in samplers]
+
+data= get_data2('RF00005',repeats=2, trainsizes=[10,20])
+samplers = make_samplers_rna()
+
+print run_experiments(data,samplers)
+
+
+
