c854976c6bfbc9709d5bb28fe80c115e7d57a272
S Mautner <mautner@cs.uni-freiburg.de>
2017-05-12 19:32:48 +0900

    add clustascript

---
+++ rnaclustscript.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#!/scratch/bi01/mautner/miniconda2/bin/fish
+
+#$ -cwd
+#$ -l h_vmem=7G
+#$ -pe smp 8
+#$ -V    
+#$ -R y
+#source ~/.bashrc
+#source ~/stupidbash.sh   the -V sould take care of the env vars
+#source ~/setpypath.sh
+python rna_run.sh 
+
