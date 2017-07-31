71ab487d69c470b52b06eabccb567b8cc3907adc
S Mautner <mautner@cs.uni-freiburg.de>
2017-07-31 18:37:22 +0200

    asd

---
+++ rna/rnaoptimizer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#!/scratch/bi01/mautner/miniconda2/bin/fish
+
+#$ -cwd
+#$ -l h_vmem=32G
+#$ -pe smp 2
+#$ -V    
+#$ -R y
+#$ -M mautner@cs.uni-freiburg.de
+#$ -t 1-12
+#$ -m a 
+#$ -m s 
+#$ -o outt
+#$ -e erra
+
+
+# 1. run runner.py make  to generate provlems
+# 2. run runner.py count to get the number of problems 
+# 3. run runner.py #problemID 
+
+#source ~/.bashrc
+#source ~/stupidbash.sh   the -V sould take care of the env vars
+#source ~/setpypath.sh
+python optimizer.py $SGE_TASK_ID 
