c850ca983e6e2eafcf00257a793229b0396349e2
S Mautner <mautner@cs.uni-freiburg.de>
2017-08-22 14:14:36 +0200

    hayaku!

---
+++ moleLearnedLayer/shell/eval_optimizer.py
@@ -0,0 +1,8 @@
+
+
+
+from moleLearnedLayer import optimize
+import make_optimizer_task as mot
+
+optimize.report( mot.aid, mot.typ, mot.num_tries, top=5  )
+
---
+++ moleLearnedLayer/shell/make_optimizer_task.py
@@ -0,0 +1,15 @@
+
+
+
+from moleLearnedLayer import optimize, util
+aid='1834'
+size=100
+repeats=2
+
+
+# these options are read by the eval thing.. 
+typ=1 
+num_tries=5
+
+if __name__ == "__main__":
+    data = util.init_optimisation(aid=aid,size=size,repeats=repeats)
